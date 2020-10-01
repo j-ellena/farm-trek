dc32d451953b783e656f338d049882325d95ff20
j-ellena <jellena.phy@gmail.com>
2020-10-01 23:45:14 +0200

    Initialize Node + Express server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.get('/', (req, res) => res.send('server'));
+
+app.listen(PORT, () => console.log(`server is running at https://localhost:${PORT}`))
