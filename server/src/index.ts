180cc22ff3c7e19f3b4fe66f1c4eed08232cfb38
j-ellena <jellena.phy@gmail.com>
2020-10-04 23:41:12 +0200

    Add Express type to server and middleware to parse incoming requests with JSON payloads

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,15 @@
-import express from 'express'
+import express, { Express } from 'express'
 import cors from 'cors'
-import database from './db/db'
+
 import { server } from './config'
 
-const app = express()
+const app: Express = express()
 
 app.use(cors())
+app.use(express.json())
 
 const { port } = server
 
-app.get('/', (req, res) => res.send('server'));
+app.get('*', (req, res) => res.send('server'))
 
 app.listen(port, () => console.log(`server is running at https://localhost:${port}`))
