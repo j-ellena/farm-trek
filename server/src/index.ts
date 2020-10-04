569275b693c11e2914a8368e4b03066c391dbba1
j-ellena <jellena.phy@gmail.com>
2020-10-04 13:13:51 +0200

    Add CORS middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import database from './db/db'
 import { server } from './config'
 
 const app = express()
+
+app.use(cors())
+
 const { port } = server
 
 app.get('/', (req, res) => res.send('server'));
