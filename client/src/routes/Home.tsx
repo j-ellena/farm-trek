a3f86edb5eb10a67c3f8d8685350ef2c05853142
j-ellena <jellena.phy@gmail.com>
2020-10-10 00:36:28 +0200

    Add Home route and component

---
+++ client/src/routes/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../logo.png'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <code>Farm Trek</code>
+            <img src={logo} className='logo' alt='logo' />
+        </div>
+    )
+}
+
+export default Home
