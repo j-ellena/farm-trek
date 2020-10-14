c70d731a29912111b1bd284c6f3e3eb047de642e
j-ellena <jellena.phy@gmail.com>
2020-10-14 23:53:14 +0200

    Create flow to getLots with useContext

--- client/src/components/Lots/List.tsx
+++ client/src/components/Lots/List.tsx
@@ -1,6 +1,25 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext, useEffect } from 'react'
+import baseUrl from '../../apis/FarmTrekApi'
+import { LotsContext } from '../../context/LotsContext'
 
 const LotsList = () => {
+
+    const { lots, setLots } = useContext(LotsContext)
+
+    const getLots = async () => {
+        try {
+            const response = await fetch(`${baseUrl}/lots`)
+            const jsonData = await response.json()
+            setLots(jsonData)
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    useEffect(() => {
+        getLots()
+    }, [])
+
     return (
         <Fragment>
             LotsList
