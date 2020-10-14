e87906f33c85be9ea53333fcac3bf6fecd7a41c4
j-ellena <jellena.phy@gmail.com>
2020-10-14 23:56:19 +0200

    Create basic form for component to input a lot

--- client/src/components/Lots/Input.tsx
+++ client/src/components/Lots/Input.tsx
@@ -1,9 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { FormEvent, Fragment } from 'react'
 
 const LotsInput = () => {
+
+    const placeholder = 'Input new lot'
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>):void => {
+        e.preventDefault()
+    }
+
     return (
         <Fragment>
-            LotsInput
+            <form>
+                <input type='text' onChange={e => console.log(e.target.value)} placeholder={placeholder} required/>
+                <button type='submit'>Add</button>
+            </form>
         </Fragment>
     )
 }
