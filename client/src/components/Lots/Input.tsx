a0cf25771bc4a94cce18b2e8af0058c8cd575e6e
j-ellena <jellena.phy@gmail.com>
2020-10-17 23:10:49 +0200

    Implement functionality to add a lot

--- client/src/components/Lots/Input.tsx
+++ client/src/components/Lots/Input.tsx
@@ -1,17 +1,31 @@
-import React, { FormEvent, Fragment } from 'react'
+import React, { FormEvent, Fragment, useContext, useState } from 'react'
+import HTTPclient from 'src/apis/FarmTrekApi'
+import { LotsContext } from 'src/context/LotsContext'
 
 const LotsInput = () => {
 
+    const [name, setName] = useState('')
+    const { addLot } = useContext(LotsContext)
+
     const placeholder = 'Input new lot'
 
-    const onSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        try {
+            const response = await HTTPclient.post('/lots', {
+                name
+            })
+            addLot(response.data)
+            setName('')
+        } catch (err) {
+            console.error(err.message)
+        }
     }
 
     return (
         <Fragment>
-            <form>
-                <input type='text' onChange={e => console.log(e.target.value)} placeholder={placeholder} required />
+            <form onSubmit={onSubmit}>
+                <input value={name} onChange={e => setName(e.target.value)} type='text' placeholder={placeholder} required />
                 <button type='submit'>Add</button>
             </form>
         </Fragment>
--- client/src/context/LotsContext.tsx
+++ client/src/context/LotsContext.tsx
@@ -3,11 +3,15 @@ import React, { useState, createContext } from 'react'
 export const LotsContext = createContext<any>([])
 
 export const LotsProvider = (props: any) => {
-    
-    const [lots, setLots] = useState([])
+
+    const [lots, setLots] = useState<Lot[]>([])
+
+    const addLot = (lot: Lot): void => {
+        setLots([lot, ...lots])
+    }
 
     return (
-        <LotsContext.Provider value={{lots, setLots}}>
+        <LotsContext.Provider value={{ lots, setLots, addLot }}>
             {props.children}
         </LotsContext.Provider>
     )
