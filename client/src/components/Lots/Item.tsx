144c05ecaca9b232ed502b61b5567e5b77299978
j-ellena <jellena.phy@gmail.com>
2020-10-18 00:08:48 +0200

    Implement functionality to delete a lot

--- client/src/components/Lots/Item.tsx
+++ client/src/components/Lots/Item.tsx
@@ -1,11 +1,25 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
 import { ILot } from 'src/interface'
+import HTTPclient from 'src/apis/FarmTrekApi'
+import { LotsContext } from 'src/context/LotsContext'
 
 const Item: FC<ILot> = ({ lot }) => {
+
+    const { lots, setLots } = useContext(LotsContext)
+
+    const deleteLot = async (id: number) => {
+        try {
+            HTTPclient.delete(`/lots/${id}`)
+            setLots(lots.filter((lot: Lot) => lot.id !== id))
+        } catch (err) {
+            console.error(err)
+        }
+    }
 
     return (
         <div>
             {lot.name}
+            <button onClick={() => deleteLot(lot.id)} >Delete</button>
         </div>
     )
 }
