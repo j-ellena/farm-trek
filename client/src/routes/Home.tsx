6e70e7344232696ae01c5b6fea7f3d5d7be535a5
j-ellena <jellena.phy@gmail.com>
2020-10-18 15:05:21 +0200

    Create primitive navigation

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from 'src/logo.png'
 
 const Home = () => {
+
     return (
         <div className='home'>
+            <Link to='/lots'>Lots</Link>
             <code>Farm Trek</code>
             <img src={logo} className='logo' alt='logo' />
         </div>
--- client/src/routes/Lots.tsx
+++ client/src/routes/Lots.tsx
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import { LotsInput, LotsList } from 'src/components/Lots'
 
 const Lots = () => {
+
     return (
         <Fragment>
+            <Link to ='/'>Home</Link>
             <h1>Lots</h1>
             <LotsInput />
             <LotsList />
