49e425fe292240720747b1d419faa1b9b6a9fd0d
j-ellena <jellena.phy@gmail.com>
2020-10-22 00:07:54 +0200

    Add LotsEdit to the aggregated re-export related to Lots

--- client/src/components/Lots/index.ts
+++ client/src/components/Lots/index.ts
@@ -1,5 +1,6 @@
+import LotsEdit from 'src/components/Lots/Edit'
 import LotsInput from 'src/components/Lots/Input'
 import LotsItem from 'src/components/Lots/Item'
 import LotsList from 'src/components/Lots/List'
 
-export { LotsInput, LotsItem, LotsList }
+export { LotsEdit, LotsInput, LotsItem, LotsList }
