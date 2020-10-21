50e366e2c44a42b5e45bc2ce930df4c6d4e6068b
j-ellena <jellena.phy@gmail.com>
2020-10-22 00:03:00 +0200

    Replace text with translations for Home component

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 import logo from 'src/logo.png'
 
 const Home = () => {
 
+    const { t } = useTranslation()
+
     return (
         <div className='home'>
-            <Link to='/lots'>Lots</Link>
-            <code>Farm Trek</code>
+            <Link to='/lots'>{t('basic.lots')}</Link>
+            <code>{t('app.name')}</code>
             <img src={logo} className='logo' alt='logo' />
         </div>
     )
