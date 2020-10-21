0ed97f0d39e3f3b2d68644d9ef4598375062ebd7
j-ellena <jellena.phy@gmail.com>
2020-10-22 00:03:22 +0200

    Replace text with translations for Lots component

--- client/src/routes/Lots.tsx
+++ client/src/routes/Lots.tsx
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import { LotsInput, LotsList } from 'src/components/Lots'
 
 const Lots = () => {
 
+    const { t } = useTranslation()
+
     return (
         <Fragment>
-            <Link to ='/'>Home</Link>
-            <h1>Lots</h1>
+            <Link to='/'>Home</Link>
+            <h1>{t('basic.lots')}</h1>
             <LotsInput />
             <LotsList />
         </Fragment>
