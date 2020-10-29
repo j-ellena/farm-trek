4904409e20a310c4d06a0e386b4452c7ca6e21ce
j-ellena <jellena.phy@gmail.com>
2020-10-29 14:51:56 +0100

    Configure LanguageDetector options

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -1,14 +1,20 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 i18n
   .use(Backend)
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
-    lng: 'hr',
     fallbackLng: 'en',
     supportedLngs: ['hr', 'en', 'de'],
+    detection: {
+      order: ['querystring', 'cookie', 'localStorage', 'htmlTag'],
+      caches: ['localStorage', 'cookie'],
+      excludeCacheFor: ['cimode'], 
+    },
     debug: process.env.NODE_ENV === 'development',
   })
 
