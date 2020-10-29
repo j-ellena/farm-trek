2aa9707998f36bd22b8d70c55f8c9b751db7449b
j-ellena <jellena.phy@gmail.com>
2020-10-29 16:39:16 +0100

    Fix for eslint rule react/destructuring-assignment

--- client/src/context/LotsContext.tsx
+++ client/src/context/LotsContext.tsx
@@ -4,29 +4,31 @@ export const LotsContext = createContext<any>([])
 
 export const LotsProvider = (props: any) => {
 
-    const [lots, setLots] = useState<Lot[]>([])
+  const [lots, setLots] = useState<Lot[]>([])
 
-    const addLot = (lot: Lot): void => {
-        setLots([lot, ...lots])
-    }
+  const addLot = (lot: Lot): void => {
+    setLots([lot, ...lots])
+  }
 
-    const deleteLot = (id: number): void => {
-        setLots(lots.filter((lot: Lot) => lot.id !== id))
-    }
+  const deleteLot = (id: number): void => {
+    setLots(lots.filter((lot: Lot) => lot.id !== id))
+  }
 
-    const updateLot = (lot: Lot): void => {
-        const index = lots.findIndex((item) => item.id === lot.id)
-        const updatedLots = [...lots]
-        updatedLots[index] = lot
-        setLots(updatedLots)
-    }
+  const updateLot = (lot: Lot): void => {
+    const index = lots.findIndex((item) => item.id === lot.id)
+    const updatedLots = [...lots]
+    updatedLots[index] = lot
+    setLots(updatedLots)
+  }
 
-    return (
-      <LotsContext.Provider value={{
-            lots, setLots, addLot, deleteLot, updateLot,
-        }}
-      >
-        {props.children}
-      </LotsContext.Provider>
-    )
+  const { children } = props
+
+  return (
+    <LotsContext.Provider value={{
+      lots, setLots, addLot, deleteLot, updateLot,
+    }}
+    >
+      {children}
+    </LotsContext.Provider>
+  )
 }
