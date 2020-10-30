b7ef6b7ceb552a901fa25e79bef34b23878546db
j-ellena <jellena.phy@gmail.com>
2020-10-30 16:09:41 +0100

    Create theme and wrap app with ThemeProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,29 @@
 import React, { StrictMode, Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { I18nextProvider } from 'react-i18next'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import i18n from 'src/i18n'
 import { Home, Lots } from 'src/routes'
 import { LanguageSwitcher } from 'src/components/Navbar'
 import { LotsProvider } from 'src/context/LotsContext'
+import theme from 'src/theme'
 import 'src/App.css'
 
 const App = () => (
   <StrictMode>
     <Suspense fallback='...'>
       <I18nextProvider i18n={i18n}>
-        <LotsProvider>
-          <LanguageSwitcher />
-          <Router>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/lots' component={Lots} />
-            </Switch>
-          </Router>
-        </LotsProvider>
+        <MuiThemeProvider theme={theme}>
+          <LotsProvider>
+            <LanguageSwitcher />
+            <Router>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/lots' component={Lots} />
+              </Switch>
+            </Router>
+          </LotsProvider>
+        </MuiThemeProvider>
       </I18nextProvider>
     </Suspense>
   </StrictMode>
