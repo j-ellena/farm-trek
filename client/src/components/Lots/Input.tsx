0bd9ed104377165060552f5bc180291863bef459
j-ellena <jellena.phy@gmail.com>
2020-11-08 17:21:10 +0100

    Add Mui style to InputLot component

--- client/src/components/Lots/Input.tsx
+++ client/src/components/Lots/Input.tsx
@@ -5,8 +5,26 @@ import { useTranslation } from 'react-i18next'
 import HTTPclient from 'src/apis/FarmTrekApi'
 import { LotsContext } from 'src/context/LotsContext'
 
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
+import AddCircleIcon from '@material-ui/icons/AddCircle'
+import Button from '@material-ui/core/Button'
+import CancelIcon from '@material-ui/icons/Cancel'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Grid from '@material-ui/core/Grid'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    button: {
+      color: theme.palette.primary.light
+    }
+  })
+)
+
 const LotsInput: FC = () => {
 
+    const classes = useStyles()
     const { t } = useTranslation()
     const [name, setName] = useState<string>('')
     const { addLot } = useContext(LotsContext)
@@ -27,14 +45,19 @@ const LotsInput: FC = () => {
     }
 
     return (
-      <>
-        <form onSubmit={onSubmit}>
-          <input value={name} onChange={(e) => setName(e.target.value)} type='text' placeholder={placeholder} required />
-          <button type='submit'>{t('basic.add')}</button>
-          {name
-            && <button type='button' onClick={() => setName('')}>{t('basic.dismiss')}</button>}
-        </form>
-      </>
+      <Grid item>
+        <Card variant='outlined'>
+          <form onSubmit={onSubmit}>
+            <CardContent>
+              <input value={name} onChange={(e) => setName(e.target.value)} type='text' placeholder={placeholder} required />
+            </CardContent>
+            <CardActions>
+              <Button type='button' onClick={() => setName('')} startIcon={<CancelIcon />}>{t('basic.dismiss')}</Button>
+              <Button className={classes.button} type='button' startIcon={<AddCircleIcon />}>{t('basic.add')}</Button>
+            </CardActions>
+          </form>
+        </Card>
+      </Grid>
     )
 }
 
