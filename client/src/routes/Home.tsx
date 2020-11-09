fc061abb28ea0a9e204cd8bf784031803eb8159d
j-ellena <jellena.phy@gmail.com>
2020-11-09 21:30:52 +0100

    Adjust Home page to dark theme

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,35 +1,23 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import logo from 'src/logo.png'
+import logoDark from 'src/logoDark.png'
 
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      backgroundColor: 'white',
-      color: theme.palette.primary.main,
-      height: '90vh'
-    },
-  })
-)
-
 const Home = () => {
 
-  const classes = useStyles()
   const { t } = useTranslation()
 
   return (
-    <Grid item container direction='column' alignContent='center' alignItems='center' className={classes.root}>
+    <>
       <Grid item>
         <Typography variant='h1'>{t('app.name')}</Typography>
       </Grid>
       <Grid item>
-        <img src={logo} className='logo' alt='logo' />
+        <img src={logoDark} className='logo' alt='logo' />
       </Grid>
-    </Grid>
+    </>
   )
 }
 
