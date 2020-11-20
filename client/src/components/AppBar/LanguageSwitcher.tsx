851ffefc34c4de1e609826fbf276d0a142f16344
j-ellena <jellena.phy@gmail.com>
2020-11-20 23:58:08 +0100

    Stlye LanguageSwitcher

--- client/src/components/AppBar/LanguageSwitcher.tsx
+++ client/src/components/AppBar/LanguageSwitcher.tsx
@@ -1,27 +1,63 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+import LanguageIcon from '@material-ui/icons/Language'
+import Select from '@material-ui/core/Select'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      '& .MuiFormControl-root': {
+        height: '25px',
+      },
+      '& .MuiInputLabel-formControl': {
+        left: '25%',
+      }
+    }
+  }),
+)
+
 const LanguageSwitcher = () => {
 
+  const classes = useStyles()
   const { i18n } = useTranslation()
   const { supportedLngs } = i18n.options
+  const handleChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+    i18n.changeLanguage(e.target.value as string)
+    document.documentElement.lang = e.target.value as string
+  }
 
   return (
     <div>
       {
         supportedLngs
         && (
-          <select
-            value={i18n.language}
-            onChange={(e) => {
-              i18n.changeLanguage(e.target.value)
-              document.documentElement.lang = e.target.value
-            }}
-          >
-            {supportedLngs
-              .filter((lng: string) => lng !== 'cimode')
-              .map((lng: string) => <option key={lng} value={lng}>{lng.toUpperCase()}</option>)}
-          </select>
+          <>
+            <FormControl
+              variant='outlined'
+              className={classes.formControl}
+            >
+              <InputLabel htmlFor='outlined-age-native-simple'><LanguageIcon /></InputLabel>
+              <Select
+                native
+                value={i18n.language}
+                onChange={handleChange}
+                label='language'
+                inputProps={{
+                  name: 'language',
+                  id: 'outlined-age-native-simple',
+                }}
+              >
+                {supportedLngs
+                  .filter((lng: string) => lng !== 'cimode')
+                  .map((lng: string) => <option key={lng} value={lng}>{lng.toUpperCase()}</option>)}
+              </Select>
+            </FormControl>
+          </>
         )
       }
     </div>
