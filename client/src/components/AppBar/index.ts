5614c0583d81d790ed83c0e8f1ac51a83e019e6a
j-ellena <jellena.phy@gmail.com>
2020-11-27 23:49:57 +0100

    Extract styles from ThemeSwitcher as a custom hook

--- client/src/components/AppBar/index.ts
+++ client/src/components/AppBar/index.ts
@@ -1,5 +1,6 @@
 import Header from 'src/components/AppBar/Header'
 import LanguageSwitcher from 'src/components/AppBar/LanguageSwitcher'
 import ThemeSwitcher from 'src/components/AppBar/ThemeSwitcher'
+import useThemeSwitcherStyle from 'src/components/AppBar/useThemeSwitcherStyle'
 
-export { Header, LanguageSwitcher, ThemeSwitcher }
+export { Header, LanguageSwitcher, ThemeSwitcher, useThemeSwitcherStyle }
---
+++ client/src/components/AppBar/useThemeSwitcherStyle.ts
@@ -0,0 +1,31 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import { yellow, indigo, blue, lightBlue } from '@material-ui/core/colors'
+import { dark } from 'src/theme'
+
+const gradientDark = `linear-gradient(${indigo[900]}, ${blue[600]})`
+const gradientLight = `linear-gradient(${lightBlue[900]}, ${lightBlue[300]})`
+const transition = 'all 0.7s linear'
+const visible = 'translate(0, 0)'
+const hiddenAbove = 'translate(25px, -100px)'
+const hiddenBelow = 'translate(-25px, 100px)'
+
+const useToggleStyle = makeStyles((theme: Theme) => createStyles({
+    toggleButton: {
+        background: theme.palette.type === dark ? gradientDark : gradientLight,
+        borderRadius: theme.spacing(4),
+        height: theme.spacing(4),
+        overflow: 'hidden',
+    },
+    darkIcon: {
+        color: yellow[800],
+        transition,
+        transform: theme.palette.type === dark ? visible : hiddenAbove
+    },
+    lightIcon: {
+        color: yellow[500],
+        transition,
+        transform: theme.palette.type === dark ? hiddenBelow : visible
+    }
+}))
+
+export default useToggleStyle
