655b67dc15561f5297a273b7379b4b32bf4fd591
j-ellena <jellena.phy@gmail.com>
2020-11-28 23:23:45 +0100

    Nest ThemeSwitcher into MuiTooltip to add informative text

--- client/src/components/AppBar/ThemeSwitcher.tsx
+++ client/src/components/AppBar/ThemeSwitcher.tsx
@@ -1,21 +1,28 @@
 import React, { useContext } from 'react'
+import { useTranslation } from 'react-i18next'
 import { ThemeContext } from 'src/context'
 import { useThemeSwitcherStyle } from 'src/components/AppBar'
 
 import IconButton from '@material-ui/core/IconButton'
 import NightsStayIcon from '@material-ui/icons/NightsStay'
+import Tooltip from '@material-ui/core/Tooltip'
 import WbSunnyIcon from '@material-ui/icons/WbSunny'
 
 const ThemeSwitcher = () => {
 
+  const { t } = useTranslation()
   const { toggleTheme } = useContext(ThemeContext)
   const classes = useThemeSwitcherStyle()
 
+  const title = t('theme.toggleTheme')
+
   return (
-    <IconButton className={classes.toggleButton} onClick={() => toggleTheme()}>
-      <NightsStayIcon className={classes.darkIcon} />
-      <WbSunnyIcon className={classes.lightIcon} />
-    </IconButton>
+    <Tooltip title={title} aria-label={title}>
+      <IconButton className={classes.toggleButton} onClick={() => toggleTheme()}>
+        <NightsStayIcon className={classes.darkIcon} />
+        <WbSunnyIcon className={classes.lightIcon} />
+      </IconButton>
+    </Tooltip>
   )
 }
 
