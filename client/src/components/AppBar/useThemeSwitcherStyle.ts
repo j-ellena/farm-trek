4b70c4999d2edd3f33f97d57e972fbc07cbe2339
j-ellena <jellena.phy@gmail.com>
2020-11-30 19:32:45 +0100

    Rename useToggleStyle const to be consistent with useThemeSwitcherStyle custom hook

--- client/src/components/AppBar/useThemeSwitcherStyle.ts
+++ client/src/components/AppBar/useThemeSwitcherStyle.ts
@@ -5,11 +5,12 @@ import { dark } from 'src/theme'
 const gradientDark = `linear-gradient(${indigo[900]}, ${blue[600]})`
 const gradientLight = `linear-gradient(${lightBlue[900]}, ${lightBlue[300]})`
 const transition = 'all 0.7s linear'
-const visible = 'translate(0, 0)'
+
+const visiblePosition = 'translate(0, 0)'
 const hiddenAbove = 'translate(25px, -100px)'
 const hiddenBelow = 'translate(-25px, 100px)'
 
-const useToggleStyle = makeStyles((theme: Theme) => createStyles({
+const useThemeSwitcherStyle = makeStyles((theme: Theme) => createStyles({
     toggleButton: {
         background: theme.palette.type === dark ? gradientDark : gradientLight,
         borderRadius: theme.spacing(4),
@@ -19,13 +20,13 @@ const useToggleStyle = makeStyles((theme: Theme) => createStyles({
     darkIcon: {
         color: yellow[800],
         transition,
-        transform: theme.palette.type === dark ? visible : hiddenAbove
+        transform: theme.palette.type === dark ? visiblePosition : hiddenAbove
     },
     lightIcon: {
         color: yellow[500],
         transition,
-        transform: theme.palette.type === dark ? hiddenBelow : visible
+        transform: theme.palette.type === dark ? hiddenBelow : visiblePosition
     }
 }))
 
-export default useToggleStyle
+export default useThemeSwitcherStyle
