7044251826433d15a50028555c8cbfd8e21b26c4
j-ellena <jellena.phy@gmail.com>
2020-12-05 22:35:07 +0100

    Nest LanguageSwitcher into MuiTooltip to add informative text

--- client/src/components/AppBar/LanguageSwitcher.tsx
+++ client/src/components/AppBar/LanguageSwitcher.tsx
@@ -6,13 +6,15 @@ import FormControl from '@material-ui/core/FormControl'
 import InputLabel from '@material-ui/core/InputLabel'
 import LanguageIcon from '@material-ui/icons/Language'
 import Select from '@material-ui/core/Select'
+import Tooltip from '@material-ui/core/Tooltip'
 
 const LanguageSwitcher = () => {
 
   const classes = useStylesLanguageSwitcher()
-  const { i18n } = useTranslation()
+  const { i18n, t } = useTranslation()
   const { supportedLngs } = i18n.options
-  
+
+  const title = t('language.switchLanguage')
   const handleChange = (e: React.ChangeEvent<{ value: unknown }>) => {
     i18n.changeLanguage(e.target.value as string)
     document.documentElement.lang = e.target.value as string
@@ -21,25 +23,27 @@ const LanguageSwitcher = () => {
   return (
     <>
       {supportedLngs && (
-        <FormControl className={classes.formControl} variant='outlined'>
-          <InputLabel htmlFor='language-switcher'>
-            <LanguageIcon />
-          </InputLabel>
-          <Select
-            native
-            value={i18n.language}
-            onChange={handleChange}
-            label='language'
-            inputProps={{
-              name: 'language',
-              id: 'language-switcher',
-            }}
-          >
-            {supportedLngs
-              .filter((lng: string) => lng !== 'cimode')
-              .map((lng: string) => <option key={lng} value={lng}>{lng.toUpperCase()}</option>)}
-          </Select>
-        </FormControl>
+        <Tooltip title={title} aria-label={title}>
+          <FormControl className={classes.formControl} variant='outlined'>
+            <InputLabel htmlFor='language-switcher'>
+              <LanguageIcon />
+            </InputLabel>
+            <Select
+              native
+              value={i18n.language}
+              onChange={handleChange}
+              label='language'
+              inputProps={{
+                name: 'language',
+                id: 'language-switcher',
+              }}
+            >
+              {supportedLngs
+                .filter((lng: string) => lng !== 'cimode')
+                .map((lng: string) => <option key={lng} value={lng}>{lng.toUpperCase()}</option>)}
+            </Select>
+          </FormControl>
+        </Tooltip>
       )}
     </>
   )
